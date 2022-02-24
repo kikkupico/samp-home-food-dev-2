87ffee335ee274c2dd8869af1062f7f43c2e8622
Ram <ram@hasura.io>
2022-02-24 18:06:32 +0530

    deliveries

---
+++ hasura-data/migrations/default/1645704226419_create_table_public_deliveries/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."deliveries" ("id" serial NOT NULL, "order" integer NOT NULL, "location" text NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("order") REFERENCES "public"."orders"("id") ON UPDATE restrict ON DELETE restrict);
